fc16a5b6f6fdccb02b774f539493696524d2a570
njp <n.pearman@gmail.com>
2016-07-26 19:35:58 +0200

    extracted dedicated renderer

--- app/components/WorkoutChart.js
+++ app/components/WorkoutChart.js
@@ -2,31 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Chart from 'chart.js';
 import styles from './WorkoutChart.css';
+import ChartRenderer from '../utils/ChartRenderer.js'
 
 export default class WorkoutChart extends Component {
-  width(data, currentIndex) {
-    return data[currentIndex + 1].start - data[currentIndex].start
-  }
-  yStart(ftp) {
-    let fullHeight = document.getElementById('workout').clientHeight
-    return fullHeight - ftp 
-  }
-  height(ftp) {
-    return ftp 
-  }
-  drawCanvas() {
-    let rawCanvas = document.getElementById('workout').getContext('2d')
-    rawCanvas.fillStyle = 'rgba(255,99,132,0.8)'
-    let rawWorkout = this.props.workout.data
-    let workoutLength = rawWorkout[rawWorkout.length - 1].start
-    let sectionFactor = document.getElementById('workout').clientWidth / workoutLength
-    let workout = rawWorkout.map((data) => { return { 'start': Math.floor(data.start * sectionFactor), 'ftp': (data.ftp * 100) } })
-    for (let i = 0; i < workout.length - 1; i++) {
-      rawCanvas.fillRect(workout[i].start, this.yStart(workout[i].ftp), this.width(workout, i), workout[i].ftp)
-    }
-  }
   componentDidMount() {
-    this.drawCanvas()
+    new ChartRenderer(this.props.workout.data, document.getElementById('workout')).drawCanvas()
   }
   render() {
     return (
---
+++ app/utils/chartRenderer.js
@@ -0,0 +1,35 @@
+export default class ChartRenderer {
+  constructor(workoutData, canvasContainer) {
+    this.rawData = workoutData
+    this.dataEnd = this.rawData[this.rawData.length - 1]
+    this.canvasContainer = canvasContainer
+  }
+  width(data, currentIndex) {
+    return data[currentIndex + 1].start - data[currentIndex].start
+  }
+  yStart(ftp) {
+    return this.fullHeight - ftp 
+  }
+  height(ftp) {
+    return ftp 
+  }
+  renderText(canvas) {
+    let totalTime = this.dataEnd.start / 60
+    let totalTimeText = Math.floor(totalTime).toString() + ' mins'
+    canvas.font = '20px Arial' 
+    canvas.fillStyle = 'rgba(55,9,32,0.8)'
+    canvas.fillText(totalTimeText, 15, this.fullHeight - 10)
+  }
+  drawCanvas() {
+    let rawCanvas = this.canvasContainer.getContext('2d')
+    rawCanvas.fillStyle = 'rgba(255,99,132,0.8)'
+    let workoutLength = this.dataEnd.start
+    let sectionFactor = this.canvasContainer.clientWidth / workoutLength
+    let workout = this.rawData.map((data) => { return { 'start': Math.floor(data.start * sectionFactor), 'ftp': (data.ftp * 100) } })
+    this.fullHeight = this.canvasContainer.clientHeight
+    for (let i = 0; i < workout.length - 1; i++) {
+      rawCanvas.fillRect(workout[i].start, this.yStart(workout[i].ftp), this.width(workout, i), workout[i].ftp)
+    }
+    this.renderText(rawCanvas)
+  }
+}
