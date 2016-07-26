78291640409cfe41167b83e80c67cafe49df768c
njp <n.pearman@gmail.com>
2016-07-26 22:02:30 +0200

    actually using redux for the chart container

--- app/containers/ChartPage.js
+++ app/containers/ChartPage.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import WorkoutChart from '../components/WorkoutChart';
 import Spiral from '../data/DownwardSpiral.json';
 
 function mapStateToProps(state) {
-  return {
-
-  };
+  return {}
 }
-export default class ChartPage extends Component {
+function mapDispatchToProps() {
+  return {}
+}
+class ChartPage extends Component {
   render() {
-    return (
-      <WorkoutChart workout={Spiral} title={'Downward Spiral'} />
-    );
+    return (<WorkoutChart workout={Spiral} title={'Downward Spiral'} />)
   }
 }
+export default connect(mapStateToProps, mapDispatchToProps)(ChartPage)
