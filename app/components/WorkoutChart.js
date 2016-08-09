a5bc787ee52c3b6a100c7381b9bb0098167b089f
sergeyb <adsf>
2016-08-09 18:30:53 +0300

    Add color to props, add some styling

--- app/components/WorkoutChart.js
+++ app/components/WorkoutChart.js
@@ -5,11 +5,11 @@ import styles from './Home.css';
 class Chart extends Component {
 
   static propTypes = {
-    // color: '',
     arrStart: React.PropTypes.array,
     arrFtp: React.PropTypes.array,
     width: React.PropTypes.number,
-    height: React.PropTypes.number
+    height: React.PropTypes.number,
+    color: React.PropTypes.string
   }
 
   render() {
@@ -17,47 +17,53 @@ class Chart extends Component {
     const length = this.props.arrStart.length;
     let maxFtp = Math.max(...this.props.arrFtp);
     let fullWidth = 0;
-    let fullFtp = 0;
+    let sumFtp = 0;
     let xCoord = 0;
+
     for (let i = 0; i < length; i++) {
       fullWidth += this.props.arrStart[i];
-      fullFtp   += this.props.arrFtp[i];
+      sumFtp   += this.props.arrFtp[i];
     }
+
+    // create bars
     for (let i = 0; i < length; i++) {
       arr.push(<rect x={xCoord}
                      y={this.props.height - this.props.height * this.props.arrFtp[i] / maxFtp}
                      key={i}
-                     style={{fill:'blue',
-                       stroke: 'blue',
-                       height: `${this.props.height*this.props.arrFtp[i]/maxFtp}px`,
+                     style={{fill: this.props.color,
+                       stroke: this.props.color,
+                       height: `${this.props.height * this.props.arrFtp[i]/maxFtp}px`,
                        width: `${this.props.arrStart[i] * this.props.width / fullWidth}px`}}/>);
 
       xCoord += (this.props.arrStart[i] * this.props.width / fullWidth);
     }
+
+    // create text showing number of minutes
     arr.push(<text x='0' y={this.props.height - 15} fill='white' key='time'>{`${length} MIN`}</text>)
+
+    // create line showing average ftp
     arr.push(<line x1='0'
-                   y1={this.props.height * (1 - fullFtp / (length * maxFtp))}
+                   y1={this.props.height * (1 - sumFtp / (length * maxFtp))}
                    x2={fullWidth}
-                   y2={this.props.height * (1 - fullFtp / (length * maxFtp))}
+                   y2={this.props.height * (1 - sumFtp / (length * maxFtp))}
                    key='avgLine'
                    style={{stroke:'gray', strokeWidth:2}}/>)
+
     return (<svg style={{width: this.props.width, height: this.props.height}}>{arr}</svg>);
   }
 }
 
 export default class WorkoutChart extends Component {
 
-  // static propTypes = {
-  //   data: React.PropTypes.array
-  // }
+  static propTypes = {
+    data: React.PropTypes.array
+  }
 
-  constructor(props) {
-    super(props);
+  state = {containerWidth: window.innerWidth/2, containerHeight: window.innerHeight/3}
+
+  constructor() {
+    super();
     this.handleResize = this.handleResize.bind(this)
-    this.state = {
-      containerWidth: window.innerWidth/2,
-      containerHeight: window.innerHeight/3
-    }
   }
 
   handleResize(e) {
@@ -91,7 +97,8 @@ export default class WorkoutChart extends Component {
           <Chart arrStart={arrStart}
                  arrFtp={arrFtp}
                  width={this.state.containerWidth}
-                 height={this.state.containerHeight}/>
+                 height={this.state.containerHeight}
+                 color='blue'/>
         </div>
       </div>
     );
